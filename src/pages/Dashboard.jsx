f7c7f4e5873aaca389f4e2345d810c9ed737661f
unknown <arumansalik45@gmail.com>
2025-09-26 23:32:42 +0530

    Font Family Changed

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,21 @@
-
+import {useContext, useEffect} from "react";
+import { AuthContext} from "../context/AuthContext";
+import Navbar from "../components/Navbar";
 
 export default function Dashboard() {
+
+    const { user } = useContext(AuthContext);
+
     return (
-        <div>Dashboard</div>
+        <div className="min-h-screen bg-black text-white">
+            <Navbar />
+
+            <div className="max-w-5xl mx-auto px-6 py-10">
+                <h2 className="text-3xl font-bold mb-4">
+                    Welcome back, {user?.username} !
+                </h2>
+            </div>
+        </div>
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html", // for Vite
-    "./src/**/*.{js,ts,jsx,tsx}" // for React
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
