e0c6c7d6cbb7e933c68909bed67e5a626ade5c96
unknown <arumansalik45@gmail.com>
2025-11-25 23:01:12 +0530

    Added new route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import ProtectedRoute from "./components/ProtectedRoute";
+
 import Dashboard from "./pages/Dashboard";
 import CreatePost from "./pages/CreatePost";
 import SinglePost from "./pages/SinglePost";
@@ -9,27 +11,39 @@ import ProfilePage from "./pages/ProfilePage";
 import EditProfile from "./pages/EditProfile";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        {/*<Route path="/" element={<HomePage />} />*/}
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                    <Dashboard />
-                </ProtectedRoute>
-              }
-          />
-          <Route path="/create" element={<CreatePost />} />
-          <Route path="/posts/:id" element={<SinglePost />} />
-          <Route path="/profile/:username" element={<ProfilePage />} />
-          <Route path="/edit-profile" element={<EditProfile />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+
+                {/* PUBLIC ROUTES */}
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/posts/:id" element={<SinglePost />} />
+                <Route path="/profile/:username" element={<ProfilePage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/login" element={<LoginPage />} />
+
+                {/* PROTECTED ROUTES */}
+                <Route
+                    path="/create"
+                    element={
+                        <ProtectedRoute>
+                            <CreatePost />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/edit-profile"
+                    element={
+                        <ProtectedRoute>
+                            <EditProfile />
+                        </ProtectedRoute>
+                    }
+                />
+
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
